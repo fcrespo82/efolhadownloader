247efd571a333adcf5bf2de9689b787ce918c344
Fernando Crespo <fernando82@gmail.com>
2014-10-27 18:52:24 -0200

    Fixed some unicode stuff

--- download.py
+++ download.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+from __future__ import unicode_literals
+from __future__ import print_function
+
 import sys
 sys.dont_write_bytecode = True
 import requests
@@ -15,14 +18,14 @@ from secrets import config
 
 
 Tipo = {
-    1: u'normal',
-    3: u'suplementar',
-    4: u'13º salário'
+    1: 'normal',
+    3: 'suplementar',
+    4: '13º salário'
 }
 
-NORMAL = Tipo[1] #(1, u'normal')
-SUPLEMENTAR = Tipo[3] #(3, u'suplementar')
-DECIMO_TERCEIRO = Tipo[4] #(4, u'13º salário')
+NORMAL = Tipo[1] #(1, 'normal')
+SUPLEMENTAR = Tipo[3] #(3, 'suplementar')
+DECIMO_TERCEIRO = Tipo[4] #(4, '13º salário')
 
 
 def recupera_nome_e_cliente(session, folha_dict, cookie):
@@ -32,11 +35,11 @@ def recupera_nome_e_cliente(session, folha_dict, cookie):
     bs = BeautifulSoup(r.text)
     cliente = bs.findAll('nobr')[0].text.strip()
     nome = bs.findAll('left')[1].text.strip()
-    return nome.replace(u' ', u'_'), cliente.replace(u' ', u'_')
+    return nome.replace(' ', '_'), cliente.replace(' ', '_')
 
 
 s = requests.session()
-url_cookie = 'http://www.e-folha.sp.gov.br/desc_dempagto/entrada.asp?cliente={}'.format(str(config['cliente']).rjust(3, '0'))
+url_cookie = 'http://www.e-folha.sp.gov.br/desc_dempagto/entrada.asp?cliente={}'.format(unicode(config['cliente']).rjust(3, '0'))
 url_login = 'http://www.e-folha.sp.gov.br/desc_dempagto/PesqSenha.asp'
 url_lista_folhas = 'http://www.e-folha.sp.gov.br/desc_dempagto/Folhas.asp'
 url_download = 'http://www.e-folha.sp.gov.br/desc_dempagto/DemPagtoP.asp'
@@ -64,17 +67,17 @@ cliente = ''
 for pdf in pdfs:
     valores = pdf['onclick'][10:-3].split('\',\'')
 
-    _tipo = str(int(valores[0]))
-    _sequencia = str(valores[1]).rjust(2, '0')
-    _mesref = str(valores[2]).rjust(2, '0')
-    _anoref = str(valores[3])
+    _tipo = unicode(int(valores[0]))
+    _sequencia = unicode(valores[1]).rjust(2, '0')
+    _mesref = unicode(valores[2]).rjust(2, '0')
+    _anoref = unicode(valores[3])
 
     detalhes = {
-        u'Folha': u'Folha ref {0}/{1} tipo {2}'.format(_mesref, _anoref, Tipo[int(_tipo)]),
-        u'Tipo': _tipo,
-        u'sequencia': _sequencia,
-        u'mesref': _mesref,
-        u'anoref': _anoref
+        'Folha': 'Folha ref {0}/{1} tipo {2}'.format(_mesref, _anoref, Tipo[int(_tipo)]),
+        'Tipo': _tipo,
+        'sequencia': _sequencia,
+        'mesref': _mesref,
+        'anoref': _anoref
     }
 
     if nome == '' or cliente == '':
@@ -82,10 +85,10 @@ for pdf in pdfs:
         nome, cliente = recupera_nome_e_cliente(s, detalhes, cookies)
 
     detalhes.update({
-        u'arquivo': u'{0}_{1}-Pagamentox-{3}-{4}_{5}_{2}.pdf'.format(_anoref, _mesref, Tipo[int(_tipo)], nome, cliente, _sequencia),
-        u'nome': nome,
-        u'cliente': cliente,
-        u'description': u'{0}_{1}-{3}-{4}_{5}_{2}.pdf'.format(_anoref, _mesref, Tipo[int(_tipo)], nome.split('_')[0], cliente.split('_')[-1], _sequencia)
+        'arquivo': '{0}_{1}-Pagamentox-{3}-{4}_{5}_{2}.pdf'.format(_anoref, _mesref, Tipo[int(_tipo)], nome, cliente, _sequencia),
+        'nome': nome,
+        'cliente': cliente,
+        'description': '{0}_{1}-{3}-{4}_{5}_{2}.pdf'.format(_anoref, _mesref, Tipo[int(_tipo)], nome.split('_')[0], cliente.split('_')[-1], _sequencia)
     })
 
     folhas.append(detalhes)
@@ -114,8 +117,8 @@ for folha in folhas:
     if folha['arquivo'] not in already_downloaded: #os.path.exists(full_path_download):
         downloaded.append(folha['arquivo'])
         r = s.post(url_download, stream = True, data = folha, cookies = cookies)
-        msg = u'Arquivo: {}'.format(folha['description'])
-        final = u' - baixando'
+        msg = 'Arquivo: {}'.format(folha['description'])
+        final = ' - baixando'
         print(msg + final.rjust(80-len(msg)))
         with open(full_path_download, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
@@ -123,8 +126,8 @@ for folha in folhas:
                     f.write(chunk)
                     f.flush()
     else:
-        msg = u'Arquivo: {}'.format(folha['description'])
-        final = u' - já existe'
+        msg = 'Arquivo: {}'.format(folha['description'])
+        final = ' - já existe'
         print(msg + final.rjust(80-len(msg)))
 
 if len(downloaded) > 0:
