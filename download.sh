e1ab1bfdecd962504fa92c412ba926e876aa8ae6
Fernando Crespo <fernando82@gmail.com>
2019-02-05 15:39:48 -0200

    Improvements to the login and download

--- download.sh
+++ download.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-read -p "Matricula: " matricula 
+read -p "Matricula: " matricula
+matricula=$(printf "%06d" $matricula)
 read -s -p "Senha: " senha
 echo
 
-folhas=$(http --form POST https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/Folhas.asp txtMatricula=$matricula txtNPA=000000000 txtSenha=$senha Cookie:ASPSESSIONIDCUQRQDDT=IAGPCNHDOHPBPJOINKJHKMLC | grep -o Atualiza\(.\*\)\; | grep -o \'.\*\')
+rm /tmp/efolhasession
+# Cookie
+http --session /tmp/efolhasession https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/entrada.asp?cliente=050 &> /dev/null
+echo "Got Cookie?"
+
+# Login
+http --session /tmp/efolhasession --form POST https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/PesqSenha.asp txtMatricula=$matricula txtNPA=000000000 txtSenha=$senha btOK=ENTRAR &> /dev/null
+echo "Autenticado"
+
+# Folhas
+folhas=$(http --session /tmp/efolhasession --form POST https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/Folhas.asp | grep -o Atualiza\(.\*\)\; | grep -o \'.\*\')
+echo "Listando folhas"
 
 for folha in $folhas; do
 	tipo=$(echo $folha | cut -d, -f1 | tr -d "'")
@@ -13,5 +25,11 @@ for folha in $folhas; do
 	anoref=$(echo $folha | cut -d, -f4 | tr -d "'")
 	strTarget=$(echo $folha | cut -d, -f5 | tr -d "'")
 
-	http --form POST https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/DemPagtoP.asp Tipo=$tipo sequencia=$sequencia mesref=$mesref anoref=$anoref btOK=ENTRAR strTarget=$strTarget txtMatricula=$matricula txtNPA=000000000 txtSenha=$senha Cookie:ASPSESSIONIDCUQRQDDT=IAGPCNHDOHPBPJOINKJHKMLC > $anoref-$mesref-$tipo-$matricula.pdf
+	if [ -f $anoref-$mesref-$tipo-$matricula.pdf ]; then
+		echo "Pulando $anoref-$mesref-$tipo-$matricula.pdf"
+		continue
+	else
+		echo "Baixando $anoref-$mesref-$tipo-$matricula.pdf"
+		http --session /tmp/efolhasession --form POST --timeout 60 https://www.e-folha.prodesp.sp.gov.br/desc_dempagto/DemPagtoP.asp Tipo=$tipo sequencia=$sequencia mesref=$mesref anoref=$anoref strTarget=$strTarget > $anoref-$mesref-$tipo-$matricula.pdf
+	fi
 done
